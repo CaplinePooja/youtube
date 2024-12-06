6118d6cf06e08a556141e8e2044c34ba6fc56d6f
CaplinePooja <poojaagrawal@caplineservices.com>
2024-12-06 13:16:58 +0530

    okk

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import {Videos, ChanneCard} from './';
+import { fetchFromAPI } from '../utils/fetch-from-api';
 
 const ChannelDetail = () => {
+  const [ChannelDetail, setChannelDetail] =useState(null)
+  const [videos, setVideos] = useState([])
+  
+  const {id} =useParams();
+
+  console.log(ChannelDetail, videos)
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelID=${id}&part=snippet&order=data`)
+    .then((data) => setVideos(data?.items));
+  },[id])
+
   return (
-    <div>ChannelDetail</div>
+    <div>{id}</div>
   )
 }
 
